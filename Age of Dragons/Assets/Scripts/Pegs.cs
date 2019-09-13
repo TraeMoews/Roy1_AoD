db35f43b701a651f04cffbec1e7b1f03cef924f6
Donald Thatcher <donald.thatcher@outlook.com>
2019-09-13 01:25:15 -0400

    Added Script
    Added Script for pegs to layout fuctions of color change

---
+++ Age of Dragons/Assets/Scripts/Pegs.cs
@@ -0,0 +1,47 @@
+﻿/*  ╔═════════════════════════════╡  Mech Defense Force 2019 ╞══════════════════╗            
+    ║ Authors:  Donald Thatcher          Email: donald.thatcher@outlook.com     ║
+    ╟───────────────────────────────────────────────────────────────────────────╢░ 
+    ║ Purpose:  Contorls Pegs and Territories                                   ║░
+    ║ Usage:    Handles color change and locking of Pegs and Territoties        ║░
+    ╚═══════════════════════════════════════════════════════════════════════════╝░
+       ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pegs : MonoBehaviour
+{
+
+    #region Public
+    public Rigidbody pegs;
+    public Rigidbody priPlayer;
+    public Rigidbody secPlayer;
+    #endregion
+
+    #region Private
+    private bool ghost;
+    private float hit = 0f;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ghost = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region Fuctions
+
+    public void ghostPeg()
+    {
+        //if (priPlayer )
+    }
+    #endregion
+}
