ce161eab3acb8a132d41de4cad746403edea581c
Devin Phares <55253547+dcphares@users.noreply.github.com>
2019-09-23 10:36:37 -0400

    Environmental Music
    Created a script to play radnom music track for levels

---
+++ Age of Dragons/Assets/Scripts/PlayRandomMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRandomMusic : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] track;
+    private AudioClip trackClip;
+    void Start()
+    {
+        //audioSource = gameObject.GetComponent<AudioSource>();
+        int index = Random.Range(0, track.Length);
+        trackClip = track[index];
+        audioSource.clip = trackClip;
+        audioSource.Play();
+    }
+    void Update()
+    {
+
+
+    }
+}
