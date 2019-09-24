75149026a4cc6ff673365d2a9f63618d492b12cd
OneTrvth <44561791+OneTrvth@users.noreply.github.com>
2019-09-24 14:28:49 -0400

    Pushing changes to a future power-up

---
+++ Age of Dragons/Assets/Scripts/Split_PowerUp.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Split_PowerUp : MonoBehaviour
+{
+    //Variables
+    public GameObject[] balls;
+    int ballsNo;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Fixed Update is called once input is detected
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        Vector3 ballPos = new Vector3(this.transform.position.x, this.transform.position.y, transform.position.z);
+        ballsNo = Random.Range(2, 4);
+
+        for (int i = 0; i < ballsNo + 1; i++)
+        {
+            if (collision.gameObject.name.Contains("Devin_IceBall"))
+                Instantiate(balls[0], ballPos, transform.rotation);
+
+            else if (collision.gameObject.name.Contains("Devin_FireBall"))
+                Instantiate(balls[1], ballPos, transform.rotation);
+
+        }
+
+        this.gameObject.SetActive(false);
+
+    }
+}
+
