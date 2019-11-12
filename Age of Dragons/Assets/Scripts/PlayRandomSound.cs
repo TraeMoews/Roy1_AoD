3c1173b7277734b77d8ec4e2cb8d1202c748422b
Devin Phares <55253547+dcphares@users.noreply.github.com>
2019-11-11 21:18:17 -0500

    AudioSource!
    Added all of the sounds I've been working on into the game. Added a script to play a random sound from an array when pressing H. Added that to the Master Scene for testing purposes.

---
+++ Age of Dragons/Assets/Scripts/PlayRandomSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRandomSound : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] track;
+    private AudioClip trackClip;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            int index = Random.Range(0, track.Length);
+            trackClip = track[index];
+            audioSource.clip = trackClip;
+            audioSource.Play();
+        }
+    }
+}
