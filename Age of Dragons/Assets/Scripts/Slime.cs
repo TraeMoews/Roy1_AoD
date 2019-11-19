cd654fc6575d1c69d8ff2478e38c9f58b096caae
Donald Thatcher <43966071+Bucsfanga@users.noreply.github.com>
2019-11-19 00:36:45 -0500

    Adding Green Dragon script
    -Added script for green dragon power up

---
+++ Age of Dragons/Assets/Scripts/Slime.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : MonoBehaviour
+{
+
+    #region Public
+    public GameObject slime;
+    public float speed;
+    public AudioSource Ballsource;
+    public AudioClip BallHit;
+    #endregion
+
+    #region Private
+    #endregion
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ballsource.clip = BallHit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
