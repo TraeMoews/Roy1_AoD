11c6abb0f0c4c8f7983382c6ce6dad81bd1497d7
OneTrvth <44561791+OneTrvth@users.noreply.github.com>
2019-11-24 19:55:21 -0500

    Pushing botched efforts with the power-up

---
+++ Age of Dragons/Assets/Scripts/TC_Powerup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TC_Powerup : MonoBehaviour
+{
+    private Gold playerGold;
+    public GameObject P1;
+    public GameObject P2;
+  
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player1")
+        {
+            playerGold.GetComponent<Gold>().gold = 0;
+        }
+        else if (other.tag == "Player2:")
+       {
+            playerGold.GetComponent<Gold>().gold = 0;
+       }
+        this.gameObject.SetActive(false);
+    }
+}
